92978995b6121cee404391fe7d00f4ec3834a1bc
Yaramir Bolko <yaramir.bolko@yandex.ru>
2020-12-01 00:23:10 +0300

    Initial commit

---
+++ src/lab_2/Lab_2.java
@@ -0,0 +1,19 @@
+package lab_2;
+
+    import javax.swing.*;
+
+public class Lab_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Конструируем экземпляр фрейма
+        JFrame frame = new JFrame();
+        // Задаѐм реакцию на нажатие кнопки закрытия фрейма
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Показываем фрейм на экране
+        frame.setVisible(true);
+    }
+
+}
